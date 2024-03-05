acdc607a47dcc393f5a66dc3cad97055dda3a22d
Cassel444 <150070945+Cassel444@users.noreply.github.com>
2024-03-05 18:27:47 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,33 @@
+// Напиши клас StringBuilder, який приймає один параметр initialValue — довільний рядок, який записується у приватну властивість value об'єкта, що створюється.
+
+
+
+// Оголоси наступні методи класу:
+
+class StringBuilder {
+    #value;
+    constructor(initialValue = '') {
+        this.#value = initialValue;
+    }
+    getValue() {
+        return this.#value;
+    }
+    padEnd(str) {
+        this.#value = this.#value + str;
+    }
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+    padBoth(str) {
+        this.#value = str + this.#value + str;
+    }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
